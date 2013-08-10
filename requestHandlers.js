11df2baf5cd57540f0b09656812b1979fcf9bdea
David A <dwanderton@gmail.com>
2013-08-10 12:59:06 +0000

    server now renders html page correctlyly - remember to change the content type to text/html from text/plain which just renders source. next step is to split html file into head footer and body - as between pages it is only the body that will be changing

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,21 @@
 var exec = require("child_process").exec;
+var fs = require('fs');
+var htmlfile = "index.html";
 
-function london(){
-    console.log("Request handler 'london' was called.");
-    var content = "empty";
-    
-    exec("ls -lah",function(error, stout, sterr){
-	content = stout;
-	});
-    return content;
-
+function london(response){
+    console.log("Request handler 'london' was called.");    
+    var body = fs.readFileSync(htmlfile).toString();
+  	response.writeHead(200, {"Content-Type":"text/html"});
+	response.write(body);
+	response.end();
 }
 
-function newyork() {
+
+function newyork(response) {
     console.log("Request handler 'newyork' was called.");
-    return "hello nyc";
+    response.writeHead(200, {"Content-Type":"text/plain"});
+	response.write("Hello nyc");
+	response.end();
 }
 
 exports.london = london;

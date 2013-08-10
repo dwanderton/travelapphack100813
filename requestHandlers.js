b7dfccdffedc281d1b19fe8fb6b14eeb46de0c48
David A <dwanderton@gmail.com>
2013-08-10 13:45:03 +0000

    split the app in 3 so that the head and footer are seperated from the content. Now use node module require to make the api call

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,12 +1,19 @@
 var exec = require("child_process").exec;
 var fs = require('fs');
-var htmlfile = "index.html";
+var request = require('request');
+
+var htmlhead = "head.html";
+var htmlfooter = "footer.html";
+var htmllondon = "london.html";
 
 function london(response){
-    console.log("Request handler 'london' was called.");    
-    var body = fs.readFileSync(htmlfile).toString();
-  	response.writeHead(200, {"Content-Type":"text/html"});
-	response.write(body);
+    console.log("Request handler 'london' was called.");
+    var head = fs.readFileSync(htmlhead).toString();
+    var footer = fs.readFileSync(htmlfooter).toString();
+    var body = fs.readFileSync(htmllondon).toString();
+    var htmlfile = head + body + footer;
+	response.writeHead(200, {"Content-Type":"text/html"});
+	response.write(htmlfile);
 	response.end();
 }
 

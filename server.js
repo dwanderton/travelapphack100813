38282fea304f4103b6ef70cfe21d6159dc015ebd
David A <dwanderton@gmail.com>
2013-08-10 11:00:53 +0000

    now index.js utilises the module server.js. Also the server is able to determine url path

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var http = require("http");
-
-http.createServer(function(request, response) {
-    console.log("Request received.");
+var url = require("url");
+function start() {
+  function onRequest(request,response){
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for" + pathname + " received.");
     response.writeHead(200,{"Content-Type":"text/plain"});
     response.write("Hello Hackathon!");
     response.end();
-}).listen(8080);
+  }
 
-console.log("server has started.");
+  http.createServer(onRequest).listen(8080);
+  console.log("Server is up yo!");
+}
+exports.start = start;
